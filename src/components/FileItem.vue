7152c26da1f8343d8356aaf5222b9a47f9064343
DerZade <DerZade@gmail.com>
2020-08-09 16:54:23 +0200

    Fix typos

--- src/components/FileItem.vue
+++ src/components/FileItem.vue
@@ -13,7 +13,7 @@
                 @click="showWarning"
             ></div>
             <div v-if="state === 'error'" style="grid-area: message; color: var(--color-error); text-align: right;">
-                An error occured<br/>Click <span style="text-decoration: underline; cursor: pointer;" @click="showError">here</span> to see the details
+                An error occurred<br/>Click <span style="text-decoration: underline; cursor: pointer;" @click="showError">here</span> to see the details
             </div>
             <i
                 v-if="state === 'done'"
@@ -84,7 +84,7 @@ export default class FileItemVue extends Vue {
     }
 
     private showError () {
-        alert(`The following error occured, while trying to convert your file:\n\n${this.error}\n\nWe would appreciate if you would help us make this tool even better and report the issue, by clicking the "Having Issues?"-Button in the lower right corner of the screen.`);
+        alert(`The following error occurred, while trying to convert your file:\n\n${this.error}\n\nWe would appreciate if you would help us make this tool even better and report the issue, by clicking the "Having Issues?"-Button in the lower right corner of the screen.`);
     }
 
     private showWarning () {
@@ -112,7 +112,7 @@ export default class FileItemVue extends Vue {
             if (Math.log2(data.width) % 1 !== 0 || Math.log2(data.width) % 1 !== 0) {
                 this.warning = {
                     displayText: 'Dimensions have to<br/>be powers of two',
-                    description: 'The dimensions (width and height) of PAA Images have to be powers of two. Your image has to fullfill the same conditions, that we can convert it to PAA.'
+                    description: 'The dimensions (width and height) of PAA Images have to be powers of two. Your image has to fulfill the same conditions, that we can convert it to PAA.'
                 };
                 return;
             }
