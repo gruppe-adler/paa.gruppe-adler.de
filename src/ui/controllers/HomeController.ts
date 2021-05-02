62201dd25e7b10b4ca23e8e5af8d5f24823ed25d
DerZade <DerZade@gmail.com>
2021-05-02 14:18:01 +0200

    Prevent lottie from running in background while hidden

--- src/ui/controllers/HomeController.ts
+++ src/ui/controllers/HomeController.ts
@@ -3,7 +3,11 @@ import * as lottieData from '@/assets/logo.json';
 
 export default class HomeController extends EventTarget {
     private element: HTMLElement;
-    private lottie: LottieAnimationItem;
+    private lottie = {
+        animation: null as null|LottieAnimationItem,
+        elem: null as HTMLDivElement|null,
+        resizeHandler: null as null|(() => unknown)
+    };
 
     constructor(homeElem: HTMLElement) {
         super();
@@ -12,36 +16,45 @@ export default class HomeController extends EventTarget {
         // Handle clicks for "convert files" button
         this.element.querySelector('button').addEventListener('click', () => { this.dispatchEvent(new Event('convert-files')); });
 
+        // find lottie element
+        const lottieElem: HTMLDivElement|null = this.element.querySelector('[data-grad-paa-lottie]');
+        if (lottieElem === null) {
+            // eslint-disable-next-line no-console
+            console.error('Couldn\'t find lottie element');
+            return;
+        } else {
+            lottieElem.removeAttribute('data-grad-paa-lottie');
+            this.lottie.elem = lottieElem;
+        }
+
         this.setupLottie();
     }
 
     private setupLottie() {
-        const logoElement = this.element.querySelector('[data-grad-paa-lottie]');
-        if (logoElement === null) {
-            // eslint-disable-next-line no-console
-            console.error('Couldn\'t find lottie element');
-            return;
-        }
-
-        logoElement.removeAttribute('data-grad-paa-lottie');
+        if (this.lottie.elem === null) return;
 
         // remove placeholder image
-        logoElement.innerHTML = '';
+        this.lottie.elem.innerHTML = '';
 
-        this.lottie = Lottie.loadAnimation({
-            container: logoElement,
+        this.lottie.animation = Lottie.loadAnimation({
+            container: this.lottie.elem,
             renderer: 'canvas',
             loop: true,
             autoplay: true,
             animationData: lottieData
         });
-        this.lottie.play();
+        this.lottie.animation.play();
 
-        window.addEventListener('resize', () => {
-            window.requestAnimationFrame(() => {
-                this.lottie.resize();
-            });
-        });
+        this.lottie.resizeHandler = () => window.requestAnimationFrame(() => { this.lottie.animation?.resize(); });
+        window.addEventListener('resize', this.lottie.resizeHandler);
+    }
+
+    private removeLottie() {
+        this.lottie.animation?.destroy();
+        if (this.lottie.resizeHandler !== null) window.removeEventListener('resize', this.lottie.resizeHandler);
+
+        this.lottie.animation = null;
+        this.lottie.resizeHandler = null;
     }
 
     public toggle (shown: boolean): void {
@@ -49,9 +62,10 @@ export default class HomeController extends EventTarget {
             this.element.style.display = '';
 
             window.requestAnimationFrame(() => {
-                this.lottie.resize();
+                this.setupLottie();
             });
         } else {
+            this.removeLottie();
             this.element.style.display = 'none';
         }
     }
