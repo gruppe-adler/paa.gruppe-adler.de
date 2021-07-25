6a8231f4a3bec65076d350e67fb69f7466eb6952
DerZade <DerZade@gmail.com>
2021-07-25 21:44:11 +0200

    Dev server listen on all IPs

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,8 @@ module.exports = () => {
         },
         devServer: {
             port: 3000,
-            contentBase: path.join(__dirname, 'dist')
+            contentBase: path.join(__dirname, 'dist'),
+            host: '0.0.0.0'
         },
         resolve: {
             fallback: {
